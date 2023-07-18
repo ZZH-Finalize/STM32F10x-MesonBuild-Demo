150c4dcaee4342c2a015c79f1918437508dc4831
ZZH <1462207500@qq.com>
2023-07-18 04:10:18 +0100

    add download target & update download script
    Signed-off-by: ZZH <1462207500@qq.com>

--- proj_files/download_demo.sh
+++ proj_files/download_demo.sh
@@ -1,1 +1,1 @@
-openocd -f $1 -f $2 -c init -c "reset halt; wait_halt; flash write_image erase $3 @flash_base@" -c reset -c shutdown
+openocd -f ../openocd_cfg/stm32f1x.cfg -f $1 -c init -c "reset halt; wait_halt; flash write_image erase $2 @flash_base@" -c reset -c shutdown
