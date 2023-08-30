1867320b4420f20d57c907ac6e392cb5ecfb5399
ZZH <1462207500@qq.com>
2023-08-30 04:00:24 +0100

    add arm-debugger support
    Signed-off-by: ZZH <1462207500@qq.com>

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 
 project_name=$1
 cross_file=$2
+debugger_file=$3
 
 if [ -z $project_name ]; then
     echo "Please input a project name (or press enter and just use "demo" as your project name)"
@@ -19,12 +20,22 @@ if [ ! -s $cross_file ] || [ -z $cross_file ]; then
     fi
 fi
 
+if [ ! -s $debugger_file ] || [ -z $debugger_file ]; then
+    echo "Please input a debugger file (all debugger file are in the cross_files/debugger dir)"
+    read debugger_file
+    if [ ! -s $debugger_file ] || [ -z $debugger_file ]; then
+        echo "Invalid debugger file path: " $debugger_file
+        exit -1
+    fi
+fi
+
 echo project_name: $project_name
 echo cross_file: $cross_file
+echo debugger_file: $debugger_file
 
 rm *.code-workspace
 rm -rf builddir
 sed -i "1c project('$project_name', 'c'," meson.build
-meson setup builddir --cross-file cross_files/toolchain.ini --cross-file cross_files/stm32f10x.ini --cross-file $cross_file
+meson setup builddir --cross-file cross_files/toolchain.ini --cross-file cross_files/stm32f10x.ini --cross-file $cross_file --cross-file $debugger_file
 cp builddir/*.code-workspace .
 echo "configure done.\n" `ls *.code-workspace` "is your vscode project file"
