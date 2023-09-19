8880f62f4468dc2f25b0dfbc09bdc21203553782
ZZH <1462207500@qq.com>
2023-09-19 02:51:57 +0100

    [util]: add hash function collection for string(untested)
    Signed-off-by: ZZH <1462207500@qq.com>

---
+++ util/hash/str_hash.h
@@ -0,0 +1,17 @@
+#ifndef __STR_HASH_H__
+#define __STR_HASH_H__
+
+#include <stdint.h>
+
+typedef uint32_t (*str_hash_t)(const char *str);
+
+uint32_t bkdr_hash(const char *str);
+uint32_t sdbm_hash(const char *str);
+uint32_t rs_hash(const char *str);
+uint32_t js_hash(const char *str);
+uint32_t pjw_hash(const char *str);
+uint32_t elf_hash(const char *str);
+uint32_t djb_hash(const char *str);
+uint32_t ap_hash(const char *str);
+
+#endif // __STR_HASH_H__  
