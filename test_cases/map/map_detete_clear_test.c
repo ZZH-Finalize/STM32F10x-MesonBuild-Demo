12cade031fb6ee04e58cf05243ece91b00b86d9d
ZZH <1462207500@qq.com>
2023-09-20 04:27:58 +0100

    add map delete testcase
    Signed-off-by: ZZH <1462207500@qq.com>

--- test_cases/map/map_detete_clear_test.c
+++ test_cases/map/map_detete_clear_test.c
@@ -1,14 +1,11 @@
 #include "map_test_data.h"
 
-static int map_basic_test(void *arg)
+static int map_delete_test(void *arg)
 {
     (void)arg;
-    map_t *map = map_create(31, bkdr_hash);
-    CHECK_PTR(map, -ENOMEM);
-
-    srand(rand());
 
     test_data_t *test_data = generate_test_data(TEST_DATA_SIZE);
+    map_t *map = map_create(31, bkdr_hash);
 
     FOR_I(TEST_DATA_SIZE)
     {
@@ -20,10 +17,8 @@ static int map_basic_test(void *arg)
     FOR_I(TEST_DATA_SIZE)
     {
         test_data_t *iter = &test_data[i];
-        map_value_t search_res = 0;
-        int retv = map_search(map, iter->key, &search_res);
+        int retv = map_remove(map, iter->key);
         RETURN_IF_NZERO(retv, retv);
-        RETURN_IF(search_res != iter->val, -EINVAL);
     }
 
     release_test_data(test_data);
@@ -31,4 +26,4 @@ static int map_basic_test(void *arg)
 
     return 0;
 }
-EXPORT_TEST_CASE(map_basic_test);
+EXPORT_TEST_CASE(map_delete_test);
---
+++ test_cases/map/map_test_data.c
@@ -0,0 +1,23 @@
+#include "map_test_data.h"
+
+test_data_t *generate_test_data(uint32_t size)
+{
+    test_data_t *test_data =
+        (test_data_t *)memAlloc(sizeof(test_data_t) * size, 0);
+
+    CHECK_PTR(test_data, NULL);
+
+    FOR_I(size)
+    {
+        test_data_t *iter = &test_data[i];
+        ITER_ARRAY(key_iter, iter->key)
+        {
+            *key_iter = rand() % 95 + 32;
+        }
+        iter->key[sizeof(iter->key) - 1] = '\0';
+
+        iter->val = (map_value_t)rand();
+    }
+
+    return test_data;
+}
